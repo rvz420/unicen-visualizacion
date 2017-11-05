19d65b97da6939f5a7d7773e65632c83a0eb015b
Juan Manuel <rodriguezjuanmanuelar@gmail.com>
2017-11-05 13:06:18 -0300

    galeria de imagenes by bootstrap 4

--- TP5/EntregaFinal/js/main.js
+++ TP5/EntregaFinal/js/main.js
@@ -2,16 +2,17 @@ var cb = new Codebird;
 cb.setConsumerKey("UMJrCxTpWL2JMFnEe2JZtwa7X", "lHz7kqxnzfGAsuDcAQDlRq8fWt3lMVethWmQh4dHmzbK29t9Xo");
 cb.setToken("159279216-fS4fVDM4aD0gmgwexvRob4uOLToOKKX5bnBrhe7m", "eELKFBegOv2MsvY4Brh8AuPWGPNyHKzI8sTztXc2mG8QB");
 
-let imagenes = document.getElementById('images');
+let imagenes = document.getElementById('img-container');
 let btnBuscar = document.getElementById('buscar');
 
-  document.querySelector('.searchbox [type="reset"]').addEventListener('click', function() {  this.parentNode.querySelector('input').focus();});
-console.log(document.getElementById('label').innerHTML);
+document.querySelector('.searchbox [type="reset"]').addEventListener('click', function() {  this.parentNode.querySelector('input').focus();});
 
 btnBuscar.addEventListener('click', ()=>{
+    console.log("input value",document.getElementById('label').value);
+    let busqueda = document.getElementById('label').value;
     imagenes.innerHTML = "";
     var params = {
-        q: "#galleta",
+        q: `#${busqueda}`,
         count: 200
     };
     
@@ -24,7 +25,16 @@ btnBuscar.addEventListener('click', ()=>{
         
         json.reply.statuses.forEach((status) => {
             if(status.entities.media) {
-                imagenes.innerHTML += `<img src="${status.entities.media[0].media_url}">`;  
+                imagenes.innerHTML += `<div class=" col-sm-4 col-md-4" >
+                                          <div class="card text-white bg-primary mb-3">
+                                            <img class="card-img-top" src="${status.entities.media[0].media_url}" alt="Card image cap">
+                                            <div class="card-body">
+                                            <h4 class="card-title">Card title</h4>
+                                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a
+                                                little bit longer.</p>
+                                            </div>
+                                          </div>
+                                        </div> `
             } 
                
         })
