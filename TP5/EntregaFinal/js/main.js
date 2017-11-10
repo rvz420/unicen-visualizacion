f6dd42802e5ab1a0a7b114c249ce23414b53e0c1
Juan Manuel <rodriguezjuanmanuelar@gmail.com>
2017-11-10 16:16:58 -0300

    index y main.js

--- TP5/EntregaFinal/js/main.js
+++ TP5/EntregaFinal/js/main.js
@@ -1,6 +1,7 @@
 var cb = new Codebird;
 cb.setConsumerKey("UMJrCxTpWL2JMFnEe2JZtwa7X", "lHz7kqxnzfGAsuDcAQDlRq8fWt3lMVethWmQh4dHmzbK29t9Xo");
 cb.setToken("159279216-fS4fVDM4aD0gmgwexvRob4uOLToOKKX5bnBrhe7m", "eELKFBegOv2MsvY4Brh8AuPWGPNyHKzI8sTztXc2mG8QB");
+cb.setProxy("https://cb-proxy.herokuapp.com/");
 
 let imagenes = document.getElementById('img-container');
 let btnBuscar = document.getElementById('buscar');
@@ -25,12 +26,12 @@ btnBuscar.addEventListener('click', ()=>{
         
         json.reply.statuses.forEach((status) => {
             if(status.entities.media) {
-                imagenes.innerHTML += `<div class=" col-sm-4 col-md-4" >
+                imagenes.innerHTML +=  `<div class=" col-sm-4 col-md-4" >
                                           <div class="card text-white bg-primary mb-3">
                                             <img class="card-img-top" src="${status.entities.media[0].media_url}" alt="Card image cap">
                                             <div class="card-body">
-                                            <h4 class="card-title">Card title</h4>
-                                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a
+                                              <h4 class="card-title">Card title</h4>
+                                              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a
                                                 little bit longer.</p>
                                             </div>
                                           </div>
@@ -41,3 +42,4 @@ btnBuscar.addEventListener('click', ()=>{
     })
 })
 
+// src de las imagenes   status.entities.media[0].media_url
