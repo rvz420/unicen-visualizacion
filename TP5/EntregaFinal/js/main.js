5b278389f8982a506ee144e4564c5ff5b2c502aa
Juan Manuel <rodriguezjuanmanuelar@gmail.com>
2017-11-12 21:55:55 -0300

    switch de layout terminado

--- TP5/EntregaFinal/js/main.js
+++ TP5/EntregaFinal/js/main.js
@@ -1,43 +1,87 @@
-var cb = new Codebird;
-cb.setConsumerKey("UMJrCxTpWL2JMFnEe2JZtwa7X", "lHz7kqxnzfGAsuDcAQDlRq8fWt3lMVethWmQh4dHmzbK29t9Xo");
-cb.setToken("159279216-fS4fVDM4aD0gmgwexvRob4uOLToOKKX5bnBrhe7m", "eELKFBegOv2MsvY4Brh8AuPWGPNyHKzI8sTztXc2mG8QB");
-//cb.setProxy("https://cb-proxy.herokuapp.com/");
+$(document).ready(function(){
+    var cb = new Codebird;
+    cb.setConsumerKey("UMJrCxTpWL2JMFnEe2JZtwa7X", "lHz7kqxnzfGAsuDcAQDlRq8fWt3lMVethWmQh4dHmzbK29t9Xo");
+    cb.setToken("159279216-fS4fVDM4aD0gmgwexvRob4uOLToOKKX5bnBrhe7m", "eELKFBegOv2MsvY4Brh8AuPWGPNyHKzI8sTztXc2mG8QB");
+    //cb.setProxy("https://cb-proxy.herokuapp.com/");
 
-let imagenes = document.getElementById('img-container');
-let btnBuscar = document.getElementById('buscar');
+    let imagenes = document.getElementById('img-container');
+    let btnBuscar = document.getElementById('buscar');
+    let inputRadio = document.getElementsByClassName('form-check-input');
+    let arrImg = new Array();
 
-document.querySelector('.searchbox [type="reset"]').addEventListener('click', function() {  this.parentNode.querySelector('input').focus();});
+    document.querySelector('.searchbox [type="reset"]').addEventListener('click', function () { this.parentNode.querySelector('input').focus(); });
 
-btnBuscar.addEventListener('click', ()=>{
-    console.log("input value",document.getElementById('label').value);
-    let busqueda = document.getElementById('label').value;
+    btnBuscar.addEventListener('click', () => {
+        console.log("input value", document.getElementById('label').value);
+        arrImg = [];
 
-    if(busqueda[0] === '#'){ //saca el caracter # si en la busqueda se ingresó
-        busqueda= busqueda.substring(1,busqueda.length);
-    }
-    imagenes.innerHTML = "";
-    var params = {
-        q: `#${busqueda}`,
-        count: 200
-    };
-    
-    cb.__call(
-        "search_tweets",
-        params
-    ).then((json) => {
-        console.log(json);
-        console.log(document.getElementById('label').innerHTML);
-        json.reply.statuses.forEach((status) => {
-            if(status.entities.media) {
-                let arrImg={imagen: status.entities.media[0].media_url, likes: status.favorite_count};
+        let busqueda = document.getElementById('label').value;
+        
+        if (busqueda[0] === '#') { //saca el caracter # si en la busqueda se ingresó
+            busqueda = busqueda.substring(1, busqueda.length);
+        }
+        var params = {
+            q: `#${busqueda}`,
+            count: 200
+        };
 
-                $.get('./js/list.mst', function(template) {
-                    var rendered = Mustache.render(template, {tweets:arrImg});
+        cb.__call(
+            "search_tweets",
+            params
+        ).then((json) => {
+            console.log(json);
+            console.log(document.getElementById('label').innerHTML);
+            json.reply.statuses.forEach((status) => {
+                if (status.entities.media) {
+                    arrImg.push({ imagen: status.entities.media[0].media_url, likes: status.favorite_count });
+                }
+            });
+            console.log("imagenes2 ", arrImg);
+            //layout();
+            if (inputRadio[0].checked && arrImg) {
+                console.log("grilla activa");
+                console.log(arrImg);
+                $.get('./js/card.mst', function (template) {
+                    var rendered = Mustache.render(template, { tweets: arrImg });
                     $('#img-container').append(rendered);
-                    
+    
                 });
-            } 
-               
+            }
+            else if (inputRadio[1].checked && arrImg) {
+                console.log("lista activa");
+                $.get('./js/list.mst', function (template) {
+                    var rendered = Mustache.render(template, { tweets: arrImg });
+                    $('#img-container').append(rendered);
+    
+                });
+            }
         })
     })
-})
+
+    function layout() {
+        if (inputRadio[0].checked && arrImg) {
+            console.log("grilla activa");
+            console.log(arrImg);
+            $.get('./js/card.mst', function (template) {
+                var rendered = Mustache.render(template, { tweets: arrImg });
+                $('#img-container').append(rendered);
+
+            });
+        }
+        else if (inputRadio[1].checked && arrImg) {
+            console.log("lista activa");
+            $.get('./js/list.mst', function (template2) {
+                var rendered = Mustache.render(template2, { tweets: arrImg });
+                $('#img-container').append(rendered);
+
+            });
+            console.log("salida de la lista ",arrImg);
+        }
+    }
+
+    $('.form-check-input').on('click', () => {
+        console.log("input clickeado");
+        imagenes.innerHTML=" ";
+        layout();
+    }); 
+});
