692ff7a1465c4e7c5b6e69ed3a5eaa90821234e7
Juan Manuel <rodriguezjuanmanuelar@gmail.com>
2017-11-16 18:59:13 -0300

    slider + loader

--- TP5/EntregaFinal/js/main.js
+++ TP5/EntregaFinal/js/main.js
@@ -8,16 +8,17 @@ $(document).ready(function () {
     let btnBuscar = document.getElementById('buscar');
     let inputRadio = document.getElementsByClassName('form-check-input');
     let arrImg = new Array();
+    let myIndex = 0;
 
     document.querySelector('.searchbox [type="reset"]').addEventListener('click', function () { this.parentNode.querySelector('input').focus(); });
 
     btnBuscar.addEventListener('click', () => {
         $('#form-buscar').addClass('sefueabuscar');
+        $('#loading').addClass('loader');
         console.log("input value", document.getElementById('label').value);
         arrImg = [];
-
         let busqueda = document.getElementById('label').value;
-        imagenes.innerHTML = "";
+        //imagenes.innerHTML = "";
 
         if (busqueda[0] === '#') { //saca el caracter # si en la busqueda se ingresó
             busqueda = busqueda.substring(1, busqueda.length);
@@ -31,13 +32,12 @@ $(document).ready(function () {
             "search_tweets",
             params
         ).then((json) => {
-           /* console.log(json);
-            console.log(document.getElementById('label').innerHTML);*/
             json.reply.statuses.forEach((status) => {
                 if (status.entities.media) {
                     arrImg.push({ imagen: status.entities.media[0].media_url, likes: status.favorite_count, animation: Math.floor(Math.random() *3) +1 });
                 }
             });
+            imagenes.innerHTML = "";
             layout();
         })
     })
@@ -46,8 +46,7 @@ $(document).ready(function () {
         if (inputRadio[0].checked && arrImg) {
             $.get('./js/card.mst', function (template) {
                 var rendered = Mustache.render(template, { tweets: arrImg });
-                $('#images').html(rendered);
-                
+                $('#images').html(rendered); 
             }); 
         }
         else if (inputRadio[1].checked && arrImg) {
@@ -56,8 +55,15 @@ $(document).ready(function () {
                 $('#images').html(rendered);
             });
         }
+        else if (inputRadio[2].checked && arrImg) {
+            $.get('./js/slide.mst', function (template3) {
+                var rendered = Mustache.render(template3, { tweets: arrImg });
+                $('#images').html(rendered);
+                carousel();
+            });
+        }
         $('html,body').animate({scrollTop:$('#images').offset().top},1000);
-        setTimeout(()=>{$('#form-buscar').removeClass('sefueabuscar');},1000);
+        setTimeout(()=>{$('#form-buscar').removeClass('sefueabuscar');$('#loading').removeClass('loader');},1000);
     }
 
     //checkbox de menu de layouts
@@ -65,14 +71,24 @@ $(document).ready(function () {
         if (inputRadio[0].checked){
             $('#ly-2').removeClass('fade-in-left');
             $('#ly-2').addClass('fade-out-up');
+            $('#ly-3').removeClass('fade-in-left');
+            $('#ly-3').addClass('fade-out-up');
         }
-        else{
+        else if (inputRadio[1].checked){
             $('#ly-1').removeClass('fade-in-left');
             $('#ly-1').addClass('fade-out-up');
+            $('#ly-3').removeClass('fade-in-left');
+            $('#ly-3').addClass('fade-out-up');
+        }
+        else if (inputRadio[1].checked){
+            $('#ly-1').removeClass('fade-in-left');
+            $('#ly-1').addClass('fade-out-up');
+            $('#ly-2').removeClass('fade-in-left');
+            $('#ly-2').addClass('fade-out-up');
         }
         setTimeout(()=>{
             layout();}
-        ,3000);
+        ,2000);
         
     });
 
@@ -92,4 +108,17 @@ $(document).ready(function () {
     function topFunction() {
         $('html,body').animate({scrollTop:0},1000);
     });
+
+    /*---------carrousel------------ */
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        for (i = 0; i < x.length; i++) {
+           x[i].style.display = "none";  
+        }
+        myIndex++;
+        if (myIndex > x.length) {myIndex = 1}    
+        x[myIndex-1].style.display = "block";  
+        setTimeout(carousel, 2000); // Change image every 2 seconds
+    }
 });
